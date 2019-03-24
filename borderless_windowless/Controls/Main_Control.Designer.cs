c662c9de70734f97582fbba2eda4957081d49627
damir pasic <Damir8520@gmail.com>
2019-03-24 19:57:00 +0000

    center test

--- borderless_windowless/Controls/Main_Control.Designer.cs
+++ borderless_windowless/Controls/Main_Control.Designer.cs
@@ -49,7 +49,7 @@
             this.Find_Repair_Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Find_Repair_Button.ForeColor = System.Drawing.Color.White;
             this.Find_Repair_Button.Image = ((System.Drawing.Image)(resources.GetObject("Find_Repair_Button.Image")));
-            this.Find_Repair_Button.Location = new System.Drawing.Point(264, 268);
+            this.Find_Repair_Button.Location = new System.Drawing.Point(245, 366);
             this.Find_Repair_Button.Name = "Find_Repair_Button";
             this.Find_Repair_Button.Size = new System.Drawing.Size(153, 322);
             this.Find_Repair_Button.TabIndex = 20;
@@ -67,7 +67,7 @@
             this.Create_Customer_Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Create_Customer_Button.ForeColor = System.Drawing.Color.White;
             this.Create_Customer_Button.Image = ((System.Drawing.Image)(resources.GetObject("Create_Customer_Button.Image")));
-            this.Create_Customer_Button.Location = new System.Drawing.Point(741, 268);
+            this.Create_Customer_Button.Location = new System.Drawing.Point(722, 366);
             this.Create_Customer_Button.Name = "Create_Customer_Button";
             this.Create_Customer_Button.Size = new System.Drawing.Size(153, 322);
             this.Create_Customer_Button.TabIndex = 19;
@@ -85,7 +85,7 @@
             this.Find_Customer_Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Find_Customer_Button.ForeColor = System.Drawing.Color.White;
             this.Find_Customer_Button.Image = ((System.Drawing.Image)(resources.GetObject("Find_Customer_Button.Image")));
-            this.Find_Customer_Button.Location = new System.Drawing.Point(582, 268);
+            this.Find_Customer_Button.Location = new System.Drawing.Point(563, 366);
             this.Find_Customer_Button.Name = "Find_Customer_Button";
             this.Find_Customer_Button.Size = new System.Drawing.Size(153, 322);
             this.Find_Customer_Button.TabIndex = 18;
@@ -122,7 +122,7 @@
             this.Create_Repair_Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Create_Repair_Button.ForeColor = System.Drawing.Color.White;
             this.Create_Repair_Button.Image = ((System.Drawing.Image)(resources.GetObject("Create_Repair_Button.Image")));
-            this.Create_Repair_Button.Location = new System.Drawing.Point(423, 268);
+            this.Create_Repair_Button.Location = new System.Drawing.Point(404, 366);
             this.Create_Repair_Button.Name = "Create_Repair_Button";
             this.Create_Repair_Button.Size = new System.Drawing.Size(153, 322);
             this.Create_Repair_Button.TabIndex = 16;
@@ -160,7 +160,7 @@
             this.Customers_Button.ForeColor = System.Drawing.Color.White;
             this.Customers_Button.Image = ((System.Drawing.Image)(resources.GetObject("Customers_Button.Image")));
             this.Customers_Button.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.Customers_Button.Location = new System.Drawing.Point(582, 143);
+            this.Customers_Button.Location = new System.Drawing.Point(563, 241);
             this.Customers_Button.Name = "Customers_Button";
             this.Customers_Button.Size = new System.Drawing.Size(312, 119);
             this.Customers_Button.TabIndex = 14;
@@ -178,7 +178,7 @@
             this.Repairs_Button.ForeColor = System.Drawing.Color.White;
             this.Repairs_Button.Image = ((System.Drawing.Image)(resources.GetObject("Repairs_Button.Image")));
             this.Repairs_Button.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.Repairs_Button.Location = new System.Drawing.Point(264, 143);
+            this.Repairs_Button.Location = new System.Drawing.Point(245, 241);
             this.Repairs_Button.Name = "Repairs_Button";
             this.Repairs_Button.Size = new System.Drawing.Size(312, 119);
             this.Repairs_Button.TabIndex = 13;
