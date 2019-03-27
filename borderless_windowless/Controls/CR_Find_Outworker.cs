7cf6e7b401a12482c2a71a4250d62df2e95241be
damir pasic <Damir8520@gmail.com>
2019-03-27 22:26:20 +0000

    Created "Find Outworker" Control For use Within "Create Repair". CR_Find_User()

---
+++ borderless_windowless/Controls/CR_Find_Outworker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Jewellery_Repair_Window_2019.Controls
+{
+    public partial class CR_Find_Outworker : UserControl
+    {
+        public CR_Find_Outworker()
+        {
+            InitializeComponent();
+        }
+    }
+}
