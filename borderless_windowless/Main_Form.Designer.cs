a3adf1afb5286c1e57fd9a3a47266bfe735ccd06
damir pasic <Damir8520@gmail.com>
2019-03-31 04:19:00 +0000

    Added Icon

--- borderless_windowless/Main_Form.Designer.cs
+++ borderless_windowless/Main_Form.Designer.cs
@@ -233,6 +233,7 @@
             this.Controls.Add(this.Body_Panel);
             this.Controls.Add(this.SideBar_Panel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Main_Form";
             this.Text = "Form1";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
